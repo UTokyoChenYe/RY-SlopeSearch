43ebfb48080f5d648af649363de7f71e1cbaecc7
chenye <1174295258@edu.k.u-tokyo.ac.jp>
2025-11-11 04:35:32 +0000

    [dev]add RY-32/RY-64/RY-128 word set

--- include/model/ry_sampling_word_sets.hpp
+++ include/model/ry_sampling_word_sets.hpp
@@ -153,6 +153,48 @@ inline const std::vector<std::string> ry_16_patterns = {
 "yyyyryryyrr", "yyyyryyryrr", "yyyyryyyrrr", "yyyyryyyrry", "yyyyyryryrr", "yyyyyyryyrr", "yyyyyyyryrr", "yyyyyyyyyrr"
 };
 
+// === RY 32 ===
+inline const std::vector<std::string> ry_32_patterns = {
+"rrrrrrrryyyr", "rrrrrrrryyyy", "rrrrrryrryyy", "rrrrrryryyyr", "rrrrryrrryyy", "rrrrryryyyyr", "rrrrryryyyyy", "rrrrryyryyyr", "rrrrryyryyyy", "rrrryryrryyy",
+"rrrryryryyyr", "rrrryryryyyy", "rrrryryyryyy", "rrrryyrryyyr", "rrrryyrryyyy", "rrrryyryryyy", "rrryrrrryyyr", "rrryrrrryyyy", "rrryrryryyyr", "rrryrryryyyy",
+"rrryyrrrryyy", "rrryyrrryyyr", "rrryyryrryyy", "rrryyryyryyy", "rryrrrrrryyy", "rryrrrrryyyr", "rryrrryryyyr", "rryrrryryyyy", "rryrrryyryyy", "rryrryrrryyy",
+"rryrryrryyyr", "rryrryrryyyy", "rryrryyrryyy", "rryrryyryyyr", "rryrryyryyyy", "rryryrrrryyy", "rryryrrryyyr", "rryryrrryyyy", "rryryrryryyy", "rryryryrryyy",
+"rryryryryyyr", "rryryryryyyy", "rryryyrrryyy", "rryryyrryyyr", "rryryyrryyyy", "rryyrrrrryyy", "rryyrrryryyy", "rryyrrryyyyr", "rryyrrryyyyy", "rryyrryryyyr",
+"rryyrryryyyy", "rryyryryryyy", "rryyryyrryyy", "ryrrrrrrryyy", "ryrrrrryyyyr", "ryrrrrryyyyy", "ryrrrryrryyy", "ryrrrryryyyr", "ryrrrryryyyy", "ryrrrryyryyy",
+"ryrrryrrryyy", "ryrrryrryyyr", "ryrrryrryyyy", "ryrrryryryyy", "ryrrryyrryyy", "ryrryrrrryyy", "ryrryrryryyy", "ryrryryrryyy", "ryrryryryyyr", "ryrryryryyyy",
+"ryrryryyryyy", "ryrryyryryyy", "ryryrrryryyy", "ryryrryrryyy", "ryryrryyryyy", "ryryryrrryyy", "ryryryryryyy", "ryryryyrryyy", "ryryryyryyyr", "ryryryyryyyy",
+"ryryyrrrryyy", "ryryyrryryyy", "ryryyryrryyy", "ryryyryryyyr", "ryryyryryyyy", "ryryyryyryyy", "ryyrrrrrryyy", "ryyrrrryryyy", "ryyrrryrryyy", "ryyrrryyryyy",
+"ryyrryrrryyy", "ryyrryrryyyr", "ryyrryrryyyy", "ryyrryryryyy", "ryyrryyrryyy", "ryyrryyryyyr", "ryyrryyryyyy", "ryyryrrrryyy", "ryyryrrryyyr", "ryyryrrryyyy",
+"ryyryrryryyy", "ryyryryrryyy", "ryyryryyryyy", "ryyryyrrryyy", "ryyryyryryyy", "yrrrrryrryyy", "yrrrrryryyyr", "yrrrryrrryyy", "yrrryryrryyy", "yrrryryyryyy",
+"yrrryyryryyy", "yrryyrrrryyy", "yrryyrrryyyr", "yrryyryrryyy", "yrryyryyryyy", "yryrrrrrryyy", "yryrrrrryyyr", "yryrrryyryyy", "yryrryrrryyy", "yryrryyrryyy",
+"yryryrrrryyy", "yryryrryryyy", "yryryryrryyy", "yryryyrrryyy", "yryyrrrrryyy", "yryyrrryryyy", "yryyryryryyy", "yryyryyrryyy"
+};
+
+// === RY 64 ===
+inline const std::vector<std::string> ry_64_patterns = {
+"rrrrrrrrrryyy", "rrrrrrrryryyy", "rrrrrryrryyyy", "rrrrrryryryyy", "rrrrrryyryyyy", "rrrrryrrrryyy", "rrrrryyrrryyy", "rrrrryyrryyyy", "rrrrryyryryyy", "rrrryrrrrryyy",
+"rrrryrrryryyy", "rrrryrryrryyy", "rrrryrryryyyy", "rrrryryrrryyy", "rrrryryyryyyy", "rrrryyrrrryyy", "rrrryyrryryyy", "rrrryyryrryyy", "rrryrrryrryyy", "rrryrrryyryyy",
+"rrryrryryryyy", "rrryrryyrryyy", "rrryrryyryyyy", "rrryryrryryyy", "rrryryryrryyy", "rrryryryryyyy", "rrryryryyryyy", "rrryyryryryyy", "rryrrrryryyyy", "rryrrrryyryyy",
+"rryrrryryryyy", "rryrryrrrryyy", "rryrryrrryyyy", "rryryrrrrryyy", "rryryrrrryyyy", "rryryrrryryyy", "rryryrryrryyy", "rryryyrrryyyy", "rryryyrryyyyy", "rryryyryrryyy",
+"rryryyryryyyy", "rryryyryyryyy", "rryyrrrryryyy", "rryyrrryryyyy", "rryyrrryyryyy", "rryyrryrrryyy", "rryyrryrryyyy", "rryyryrrrryyy", "rryyryryrryyy", "rryyryryyryyy",
+"ryrrrrrrrryyy", "ryrrrrrrryyyy", "ryrrrrrryryyy", "ryrrrrryrryyy", "ryrrrrryryyyy", "ryrrrrryyryyy", "ryrrrryrryyyy", "ryrrrryryryyy", "ryrrrryyrryyy", "ryrrryrrrryyy",
+"ryrrryrrryyyy", "ryrrryrryryyy", "ryrrryryrryyy", "ryrrryyrrryyy", "ryrrryyrryyyy", "ryrrryyryryyy", "ryrryrrrrryyy", "ryrryrrryryyy", "ryrryrryryyyy", "ryrryrryyryyy",
+"ryrryryrrryyy", "ryrryryrryyyy", "ryrryryryryyy", "ryrryryyryyyy", "ryrryyrrrryyy", "ryrryyrrryyyy", "ryrryyrryryyy", "ryrryyryrryyy", "ryrryyryryyyy", "ryrryyryyryyy",
+"ryryrrrrrryyy", "ryryrrrryryyy", "ryryrrryrryyy", "ryryrrryyryyy", "ryryrryrrryyy", "ryryrryryryyy", "ryryrryyrryyy", "ryryryrrryyyy", "ryryryryrryyy", "ryryryryryyyy",
+"ryryryryyryyy", "ryryryyrrryyy", "ryryryyrryyyy", "ryryryyryryyy", "ryryyrrrryyyy", "ryryyrrryryyy", "ryryyrryrryyy", "ryryyrryyryyy", "ryryyryrrryyy", "ryryyryyrryyy",
+"ryyrrrrrrryyy", "ryyrrrrrryyyy", "ryyrrrrryryyy", "ryyrrrrryyyyy", "ryyrrrryyryyy", "ryyrrryrrryyy", "ryyrrryrryyyy", "ryyrrryryryyy", "ryyrrryyrryyy", "ryyrryrrrryyy",
+"ryyrryrryryyy", "ryyrryryrryyy", "ryyrryryryyyy", "ryyrryryyryyy", "ryyrryyrryyyy", "ryyrryyryryyy", "ryyryrrrrryyy", "ryyryrrryryyy", "ryyryrryrryyy", "ryyryrryryyyy",
+"ryyryrryyryyy", "ryyryryrrryyy", "ryyryryryryyy", "ryyryyrrrryyy", "ryyryyrryryyy", "ryyryyryrryyy", "ryyryyryryyyy", "ryyryyryyryyy" 
+};
+
+// === RY 128 ===
+inline const std::vector<std::string> ry_128_patterns = {
+"rrrrrrryyryy", "rryrryyyyyyy", "rrrrrryyyryy", "rryrrryyyyyy", "rrrrryryyryy", "rryrryryyyyy", "rrrrryyryryy", "rryryrryyyyy", "rrrrryyyyryy", "rryrrrryyyyy",
+"rrrryryyyryy", "rryrrryryyyy", "rrrryyyrryyy", "rrryyrrryyyy", "rrrryyyryryy", "rryryrrryyyy", "rrryrrryyryy", "rryrryyyryyy", "rrryryryyryy", "rryrryryryyy",
+"rrryryyrryyy", "rrryyrryryyy", "rrryryyryryy", "rryryrryryyy", "rrryyrryyryy", "rryrryyrryyy", "rrryyryryryy", "rryryryrryyy", "rryrrrryyryy", "rryrryyyyryy",
+"rryrryyryryy", "rryryrryyryy"
+};
+
 
 // mapping of sampling methods to their corresponding patterns
 // inline to avoid multiple definitions
@@ -165,6 +207,9 @@ inline const std::map<std::string, std::reference_wrapper<const std::vector<std:
     {"start_ry_4_push_matches", std::cref(ry_4_push_patterns)},
     {"start_ry_4_pull_matches", std::cref(ry_4_pull_patterns)},
     {"start_ry_16_matches", std::cref(ry_16_patterns)},
+    {"start_ry_32_matches", std::cref(ry_32_patterns)},
+    {"start_ry_64_matches", std::cref(ry_64_patterns)},
+    {"start_ry_128_matches", std::cref(ry_128_patterns)},
 };
 
 // get the pattern set of the specified sampling method
